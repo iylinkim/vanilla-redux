4ef5a89d79d838b9297ae129e511420c4403330f
iylinkim <kiylin94@naver.com>
2021-04-22 22:34:31 +0900

    Refactoring

--- src/index.js
+++ src/index.js
@@ -4,15 +4,19 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 // 오직 이 함수에서만 data를 modify할 수 있음
-// reducer가 return하는 모든 것은 data가 됨
+// reducer가 return하는 모든 것은 data(state)가 됨
 const countModifier = (count = 0, action) => {
-  if (action.type === "add") {
-    return count + 1;
-  } else if (action.type === "minus") {
-    return count - 1;
-  } else {
-    return count;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 
@@ -25,11 +29,12 @@ const countStore = createStore(countModifier);
 //subscribe(): store 안에 있는 변화들을 알려줌, store에 변화가 있을 때마다 함수 호출됨
 countStore.subscribe(onChange);
 
+//action은 항상 object 형태여야함 (무조건 type을 가짐)
 const handleAdd = () => {
-  countStore.dispatch({ type: "add" });
+  countStore.dispatch({ type: ADD });
 };
 const handleMinus = () => {
-  countStore.dispatch({ type: "minus" });
+  countStore.dispatch({ type: MINUS });
 };
 
 add.addEventListener("click", handleAdd);
