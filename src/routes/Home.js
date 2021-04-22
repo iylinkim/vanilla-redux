33ddbdede135607a86d9eaa6e5eb6c33a73dd952
iylinkim <kiylin94@naver.com>
2021-04-23 00:56:58 +0900

    #3.3 mapDispatchToProps

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
+import { actionCreators } from "./store";
 
-const Home = (props) => {
-    console.log(props)
+const Home = ({ toDos, addToDo }) => {
   const [text, setText] = useState("");
   const onChange = (e) => {
     setText(e.target.value);
   };
 
   const onSubmit = (e) => {
+    addToDo(text);
     e.preventDefault();
     setText("");
   };
@@ -19,7 +20,7 @@ const Home = (props) => {
         <input type="text" value={text} onChange={onChange} />
         <button>Add</button>
       </form>
-      <ul></ul>
+      <ul>{JSON.stringify(toDos)}</ul>
     </>
   );
 };
@@ -29,8 +30,15 @@ const mapStateToProps = (state) => {
   // state: redux의 state
   // ownProps: component의 ownProps
   // return 하는 값들은 component의 props에 다 포함됨
-  return {toDos:state};
+  return { toDos: state };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addToDo: (text) => dispatch(actionCreators.addToDo(text)),
+  };
 };
 
 //connect()는 store로 부터 state를 가져다 줌, Component(Home)으로 보내는 props에 props가 추가될 수 있도록 해줌
-export default connect(mapStateToProps)(Home);
+// connect는 2개 argument를 가짐 1.mapStateToProps, 2.mapDispatchToProps
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
