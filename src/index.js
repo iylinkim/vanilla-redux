8bba9e3aa5dff45b77051c6e3544ede5a3d86b13
iylinkim <kiylin94@naver.com>
2021-04-22 22:08:17 +0900

    Store and Reducer

--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
-}
-
-const handleAdd = () => {
-  count = count + 1;
-  updateText();
-};
-const handleMinus = () => {
-  count = count - 1;
-  updateText();
+// 오직 이 함수에서만 data를 modify할 수 있음
+const countModifier = (count = 0) => {
+  return count;
 };
 
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+const countStore = createStore(countModifier);
+console.log(countStore.getState())
+
