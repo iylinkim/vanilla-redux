28d80800b07a53c685a32a7202f13872413fea72
iylinkim <kiylin94@naver.com>
2021-04-23 00:44:08 +0900

    #3.2 mapStateToProps

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
 
 const Home = (props) => {
+    console.log(props)
   const [text, setText] = useState("");
   const onChange = (e) => {
     setText(e.target.value);
@@ -22,4 +24,13 @@ const Home = (props) => {
   );
 };
 
-export default Home;
+//함수명은 반드시 mapStateToProps로 지정해야함
+const mapStateToProps = (state) => {
+  // state: redux의 state
+  // ownProps: component의 ownProps
+  // return 하는 값들은 component의 props에 다 포함됨
+  return {toDos:state};
+};
+
+//connect()는 store로 부터 state를 가져다 줌, Component(Home)으로 보내는 props에 props가 추가될 수 있도록 해줌
+export default connect(mapStateToProps)(Home);
--- src/routes/store.js
+++ src/routes/store.js
@@ -29,6 +29,4 @@ const reducer = (state = [], action) => {
 
 const store = createStore(reducer);
 
-store.subscribe()
-
 export default store;
