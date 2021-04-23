c621ba50ed2eaa1e13ce3f0e8e8e634a19425a89
iylinkim <kiylin94@naver.com>
2021-04-23 10:32:18 +0900

    #4.3 configureStore

--- src/store.js
+++ src/store.js
@@ -1,19 +1,7 @@
-import { createAction, createReducer } from "@reduxjs/toolkit";
-import { createStore } from "redux";
+import { configureStore, createAction, createReducer } from "@reduxjs/toolkit";
 
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
-
-// const reducer = (state = [], action) => {
-//   switch (action.type) {
-//     case addToDo.type:
-//       return [{ text: action.payload, id: Date.now() }, ...state]; //createAction으로 만들었기 때문에 payload 가지고 있음
-//     case deleteToDo.type:
-//       return state.filter((todo) => todo.id !== action.payload); //createAction으로 만들었기 때문에 payload 가지고 있음
-//     default:
-//       return state;
-//   }
-// };
 
 //redux toolkit을 사용하면 state를 mutate할 수 있음
 const reducer = createReducer([], {
@@ -26,7 +14,8 @@ const reducer = createReducer([], {
     state.filter((todo) => todo.id !== action.payload),
 });
 
-const store = createStore(reducer);
+//configureStore 사용하면 Redux Developer Tools 사용할 수 있음
+const store = configureStore({ reducer });
 
 export const actionCreators = {
   addToDo,
